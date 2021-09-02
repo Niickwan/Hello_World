8ea6329e4d128ca0bab6481b133e3ade723d0a9d
Niickwan <89967473+Niickwan@users.noreply.github.com>
2021-09-02 12:38:08 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    System.out.println("Hello world");
-        System.out.println("Test2");
+    	System.out.println("Hello world");
     }
 }
 
